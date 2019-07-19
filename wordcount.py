8f112b687c9cfa69ee39ae4088c58f5c9d68af4f
srdev532 <srdev79@gamil.com>
2019-07-18 19:30:07 -0500

    adding wordcount python

---
+++ wordcount.py
@@ -0,0 +1,27 @@
+stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+
+def displayInventory(stuff):
+    print("Inventory: ")
+    item_total = 0
+
+    for k,v in stuff.items():
+        print(str(v) + ' ' + str(k) )
+        item_total += v
+    print("The total item values are" + str(item_total))
+
+
+    
+def addToInventory(stuff, dragonLoot):
+    for item in dragonLoot:
+        stuff.setdefault(item,0)
+        stuff[item] += 1
+    print (stuff)
+    
+    
+displayInventory(stuff)
+
+addToInventory(stuff, dragonLoot)
+
+Hello world
